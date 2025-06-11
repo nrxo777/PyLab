f936c60b3e1c4ebc281b653c14239e411c099965
nrxo777 <noorxcalma@gmail.com>
2025-06-11 16:24:51 +0600

    Add initial implementations for swapping numbers, checking positivity, and finding the biggest number

---
+++ 2ndSemPy/08_swapping.py
@@ -0,0 +1,9 @@
+a = int(input("Enter Your First Number: "))
+b = int(input("Enter Your Second Number: "))
+
+c = a
+a = b
+b = c
+
+print("First number is: ", a)
+print("Second number is: ", b)
---
+++ 2ndSemPy/09_pos_or_neg.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the number: "))
+
+if num == 0:
+    print("The number is Zero")
+elif num > 0:
+    print("The number is Positive")
+elif num < 0:
+    print("The number is Negetive")
